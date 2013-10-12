8dd4a7019398d63fc34529541f0f100e9d198e96
Isaac Cook <isaac@simpload.com>
2013-10-12 14:33:01 -0500

    Moved docker in as a submodule, and setup jenkins shell script.

---
+++ jenkins.sh
@@ -0,0 +1,30 @@
+project="featurelet"
+branch=`git rev-parse --abbrev-ref HEAD`
+image="$project-$branch"
+
+# clone in our docker configuration for the project
+cd docker
+#docker rmi $image || true  # attempt a delete of the old image
+docker stop `cat ../cid`
+docker rm `cat ../cid`  # also delete the old container
+# build and force a full package rebuild
+docker build -rm -t="$image" .
+if [ $? != 0 ]; then
+    # do the one time setup by invoking directly
+    rm -f tmp
+    docker run -cidfile="tmp" -dns="192.168.1.1" $image
+    # Now commit that, and reset the command to something that will wait forever, holding open other processes
+    docker commit -run="{\"Cmd\": [\"/srv/$project/start.sh\", \"true\"], \"PortSpecs\" : [\"22\", \"80\"]}" `cat tmp` $image
+    cd ../
+    cid=`docker run -d -dns="192.168.1.1" "$image"`
+    echo "$cid" > cid
+    # export configs for the nginx config
+    export port=`docker port $cid 80`
+    export image
+    # populate vars and place in nginx lookup path
+    perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' ci/nginx.conf > nginx.conf
+    sudo service nginx reload
+else
+    # some kind of failure occured, so run our cleanup script
+    docker-remove-untagged
+fi
