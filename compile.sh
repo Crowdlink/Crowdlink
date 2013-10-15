3f9fcafe3149a7bc6b2117281ca7a6e6fdbedfa4
Isaac Cook <isaac@simpload.com>
2013-10-15 01:33:01 -0500

    Fixed compile step

--- compile.sh
+++ compile.sh
@@ -2,3 +2,4 @@
 recess less/bootstrap/bootstrap.less --compile --compress > static/lib/css/bootstrap.min.css
 rm -rf static/css/*
 compass compile --force --css-dir="static/css" --sass-dir="scss" --images-dir="static/img" -s compressed
+coffee -c  -o ./static/js/ ./coffee/main.coffee
