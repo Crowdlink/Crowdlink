9fe9f549b2776f310eb93b8103d6670ade208797
Isaac Cook <isaac@simpload.com>
2014-01-27 10:02:50 -0600

    Removed a bunch of old unused files

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-recess less/bootstrap/bootstrap.less --compile --compress > static/lib/css/bootstrap.min.css
-rm -rf static/css/*
-compass compile --force --css-dir="static/css" --sass-dir="scss" --images-dir="static/img" -s compressed
-coffee -c  -o ./static/js/ ./coffee/main.coffee
-coffee -c  -o ./static/js/ ./coffee/controllers.coffee
-coffee -c  -o ./static/js/ ./coffee/app.coffee
-coffee -c  -o ./static/js/ ./coffee/services.coffee
-coffee -c  -o ./static/js/ ./coffee/lib.coffee
